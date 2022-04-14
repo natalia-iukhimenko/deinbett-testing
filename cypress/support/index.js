848f8fa20cb3061e205304708b774a18156c9030
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-04-14 17:48:05 +0300

    Allure reporting configuration

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,8 @@
 import './commands'
+import '@shelex/cypress-allure-plugin';
 
 require("cypress-xpath");
+require('@shelex/cypress-allure-plugin');
 
 Cypress.Cookies.defaults({
     preserve: 'optOutAccepted',
